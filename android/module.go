LineageOS/android_build_soong

Fix android_test install path.
Test: m nothing + diff soong mk and ninja files.
Bug: 140795853
Change-Id: I3667eac951dea7e447cf73219ff89199fca9ed63

android/module.go
@@ -154,6 +154,7 @@ type ModuleContext interface {
 	CheckbuildFile(srcPath Path)
 
 	InstallInData() bool
+	InstallInTestcases() bool
 	InstallInSanitizerDir() bool
 	InstallInRecovery() bool
 	InstallBypassMake() bool
@@ -192,6 +193,7 @@ type Module interface {
 	Enabled() bool
 	Target() Target
 	InstallInData() bool
+	InstallInTestcases() bool
 	InstallInSanitizerDir() bool
 	InstallInRecovery() bool
 	InstallBypassMake() bool
@@ -829,6 +831,10 @@ func (m *ModuleBase) NoAddressSanitizer() bool {
 }
 
 func (m *ModuleBase) InstallInData() bool {
+	return false
+}
+
+func (m *ModuleBase) InstallInTestcases() bool {
 	return false
 }
 
@@ -1502,6 +1508,10 @@ func (m *ModuleBase) IsNativeBridgeSupported() bool {
 
 func (m *moduleContext) InstallInData() bool {
 	return m.module.InstallInData()
+}
+
+func (m *moduleContext) InstallInTestcases() bool {
+	return m.module.InstallInTestcases()
 }
 
 func (m *moduleContext) InstallInSanitizerDir() bool {

LucidProject/platform_build_soong

android/module.go
@@ -129,6 +129,7 @@ type ModuleContext interface {
 	AddMissingDependencies(deps []string)
 
 	InstallInData() bool
+	InstallInTestcases() bool
 	InstallInSanitizerDir() bool
 	InstallInRecovery() bool
 
@@ -185,6 +186,7 @@ type Module interface {
 	Enabled() bool
 	Target() Target
 	InstallInData() bool
+	InstallInTestcases() bool
 	InstallInSanitizerDir() bool
 	InstallInRecovery() bool
 	SkipInstall()
@@ -653,6 +655,10 @@ func (p *ModuleBase) NoAddressSanitizer() bool {
 }
 
 func (p *ModuleBase) InstallInData() bool {
+	return false
+}
+
+func (p *ModuleBase) InstallInTestcases() bool {
 	return false
 }
 
@@ -1250,6 +1256,10 @@ func (a *ModuleBase) MakeAsPlatform() {
 
 func (a *androidModuleContext) InstallInData() bool {
 	return a.module.InstallInData()
+}
+
+func (a *androidModuleContext) InstallInTestcases() bool {
+	return a.module.InstallInTestcases()
 }
 
 func (a *androidModuleContext) InstallInSanitizerDir() bool {